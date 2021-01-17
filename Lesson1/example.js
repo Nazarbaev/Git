b94956e4f5d7580974c021ff0154768102a60ecf
Samat <samatnazarbaev@gmail.com>
2021-01-17 16:30:38 +0100

    first commit

---
+++ Lesson1/example.js
@@ -0,0 +1,28 @@
+'use strict';
+var number = 5;
+var string = "Sam";
+console.log(-(4/0));
+console.log('string'*9)
+
+let person = {
+    name: "John",
+    age: 25,
+    Muslim: true,
+};
+
+console.log(person.Muslim);
+
+
+let arr = [ 'sam.png','apple.bmp','orange.jpg'];
+
+console.log(arr[2]);
+
+// alert("Hello Visitor of our website");
+
+/*let age= confirm("Are you 18 years old?");
+console.log(age);*/
+
+let answer = prompt("What is your name?","");
+
+console.log(typeof(arr));
+
